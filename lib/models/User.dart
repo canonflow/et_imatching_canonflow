0523ada8663b3547c969cc7b7396d23d002aec13
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-04-12 00:16:05 +0700

    feat(user): add some methods for high score

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -54,4 +54,92 @@ class User {
 
     prefs.setString(LocalStorageKey.USERNAME, userJson);
   }
+
+  // Find User's High Score
+  Future<int?> FindHighScore() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? highscoresJson = await prefs.getString(LocalStorageKey.HIGHSCORES);
+
+    if (highscoresJson == null) return null;
+
+    // Decode
+    List<String> highscoresListMap = jsonDecode(highscoresJson);
+
+    // Convert into list of user
+    List<User> highscores = highscoresListMap
+      .map((json) => User.fromJson(jsonDecode(json)))
+      .toList();
+
+    // return highscores
+    //     .where((user) => user.username == username)
+    //     .firstOrNull;
+
+    final match = highscores.where((user) => user.username == username);
+
+    return match.isNotEmpty ? match.first.score : null;
+  }
+
+  // Save User's High Score
+  Future<void> SaveHighScore() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final String? highscoresJson = await prefs.getString(LocalStorageKey.HIGHSCORES);
+
+    // Kalo blm ada yg main
+    if (highscoresJson == null) {
+      List<User> highscores = [User(username: username)];
+      List<String> highscoresJson = highscores
+        .map((user) => jsonEncode(user.toJson()))
+        .toList();
+
+      await prefs.setStringList(LocalStorageKey.HIGHSCORES, highscoresJson);
+    } else {
+      // Kalo udh ada yg main
+      // Cek apakah sudah pernah main sebelumnya
+
+      // Convert into list of user
+      List<String> highscoresListMap = jsonDecode(highscoresJson);
+      List<User> highscores = highscoresListMap
+          .map((json) => User.fromJson(jsonDecode(json)))
+          .toList();
+
+      final match = highscores
+        .where((user) => user.username == username);
+
+      if (match.isNotEmpty) {
+        // Udh pernah main
+        User user = match.first;
+        user.score = score;
+        user.moves = moves;
+        user.mistakes = mistakes;
+      } else {
+        // Belum pernah main
+        highscores.add(this);
+      }
+
+      // Simpan
+      final updatedJson = highscores
+        .map((user) => jsonEncode(user.toJson()))
+        .toList();
+
+      await prefs.setStringList(LocalStorageKey.HIGHSCORES, updatedJson);
+    }
+  }
+
+  // Get All High Scores
+  static Future<List<User>?> GetAllHighscores() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final String? highscoresJson = await prefs.getString(LocalStorageKey.HIGHSCORES);
+
+    if (highscoresJson == null) return null;
+
+    List<String> highscoresListMap = jsonDecode(highscoresJson);
+    
+    List<User> highscores= highscoresListMap
+      .map((user) => User.fromJson(jsonDecode(user)))
+      .toList();
+
+    return highscores;
+  }
 }
